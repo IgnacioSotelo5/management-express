0ee9a54f0d0635f34fff85890e9243565c986f16
git <IgnacioSotelo5@users.noreply.github.com>
2025-12-08 22:10:00 -0300

    feat: add invitation API endpoints

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -4,6 +4,7 @@ import { userLoginSchema, userRegisterSchema } from "@/modules/auth/auth.schema"
 import { Router } from "express";
 import { authMiddleware } from "@/shared/middlewares/authMiddleware";
 import { asyncHandler } from "@/shared/utils/async-handler";
+import { invitationSchema } from "./invitation.schema";
 
 export const authRouter = Router();
 
@@ -12,3 +13,9 @@ authRouter.post("/signup", validateSchema(userRegisterSchema), asyncHandler(Auth
 authRouter.post("/login", validateSchema(userLoginSchema), asyncHandler(AuthController.login));
 
 authRouter.get('/validate', authMiddleware, asyncHandler(AuthController.validateToken))
+
+authRouter.post('/invite', validateSchema(invitationSchema), asyncHandler(AuthController.inviteEmployee))
+
+authRouter.get('/invitations/validate', asyncHandler(AuthController.validateInvitation))
+
+authRouter.post('/invitations/accept', asyncHandler(AuthController.acceptInvitation))
