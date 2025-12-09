56ba8da783998fb71e51c707e177b27320265835
git <IgnacioSotelo5@users.noreply.github.com>
2025-12-08 22:09:29 -0300

    feat: implement invitation business logic in AuthService

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -4,6 +4,12 @@ import { UserModel } from "@/modules/users/user.model";
 import bcrypt from 'bcrypt'
 import { UnauthorizedError } from "@/shared/errors/unauthorized.error";
 import { genJWTToken } from "@/shared/utils/genToken";
+import { randomUUID } from "crypto";
+import { BASE_URL, SALT_ROUNDS } from "@/config/config";
+import { BadRequestError } from "@/shared/errors/bad-request.error";
+import { NotFoundError } from "@/shared/errors/not-found.error";
+import { GoneError } from "@/shared/errors/gone-error";
+import { ConflictError } from "@/shared/errors/conflict.error";
 
 export class AuthService{
     static async signup({name, lastName, email, password}: userRegisterDTO){
@@ -39,4 +45,55 @@ export class AuthService{
         return {sanitizedUser, token}
         
     }
+
+    static async inviteEmployee({ email, name, bakeryId, role, expiresAt }: { email: string; name: string; bakeryId: string; role: string; expiresAt: Date }) {
+        if(!name || !email || !bakeryId || !role || !expiresAt){
+            throw new BadRequestError('Missing required fields for invitation.')
+        }
+        const existingInvitation = await UserModel.existsInvitation({ email, bakeryId });
+        if (existingInvitation) {
+            throw new BadRequestError(`An invitation for ${email} to bakery ${bakeryId} already exists.`);
+        }
+
+        const token = randomUUID()
+        const hashedToken = await bcrypt.hash(token, SALT_ROUNDS)
+
+        const invitation = await UserModel.createInvitation({ email, name, bakeryId, role, token: hashedToken, expiresAt})
+
+        const invitationLink = `${BASE_URL}/invitations/accept?token=${token}`
+
+        return {
+            invitation: {
+                invitationId: invitation.id,
+                email: invitation.email,
+                name: invitation.name,
+                bakeryId: invitation.bakeryId,
+                expiresAt: invitation.expiresAt,
+            },
+            invitationLink
+        }
+    }
+
+    static async validateInvitation(token?: string) {
+        if(!token){
+            throw new BadRequestError('Invitation token is required.')
+        }
+        const invitation = await UserModel.findInvitationByToken(token)
+
+        if(!invitation) {
+            throw new NotFoundError('Invitation not found.')
+        }
+
+        if(invitation.used){
+            throw new ConflictError('Invitation has already been used.')
+        }
+
+        if(invitation?.expiresAt < new Date()){
+            throw new GoneError('Invitation has expired.')
+        }
+
+        return invitation !== null;
+    }
+
+    static async acceptInvitation(token?: string) {}
 }
